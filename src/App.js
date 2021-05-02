f331f1b60cc21c0c368c25314157f2f3e4ff764b
hendahan <hendahan110@gmail.com>
2021-05-02 11:07:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ function App() {
       <Link to="/login">
       <button>Log In Here</button>
       </Link>
+
+      <Link to="/home">
+      <button>Home page</button>
+      </Link>
     </div>
   );
 }
