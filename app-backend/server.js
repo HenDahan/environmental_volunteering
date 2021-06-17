443e4ddff581406db6ea2e3bd996114beaaddd18
TzurielYeffet <70668027+TzurielYeffet@users.noreply.github.com>
2021-06-17 23:55:21 +0300

    Add files via upload
    put all together

--- app-backend/server.js
+++ app-backend/server.js
@@ -1,24 +1,80 @@
-import express from "express";
-import mongoose  from "mongoose";
-import cors from "cors";
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+const users = require('./routes/api/users');
 
+const db = require('./config/db.config');
 
 //App config
 
 const app = express();
-const port = process.env.PORT||5000;
-const connetion_Url = "mongodb+srv://admin-Tzuriel:Qa123456@cluster0.nlmek.mongodb.net/Vapp?retryWrites=true&w=majority";
+const port = process.env.PORT || 5001;
+
+
+var corsOptions = {
+    origin: "http://localhost:5001"
+};
 //Middlewares
 
+app.use(session({
+    secret: "This is out little secret!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// required for start useing passport.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors(corsOptions));
+
 // DB config
-mongoose.connect(connetion_Url,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(console.log("mongoDB Connected... "));
+
+const User = require('./db/users.schema');
+
+passport.use(User.createStrategy());
 
 
+// serializeUser Creates cookie
+// deserializeUser reveels the cookie data
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+mongoose.connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("mongoDB Connected... ");
+}).catch(error => {
+    console.log("Error connecting", error);
+    process.exit();
+});
+
+mongoose.set("useCreateIndex", true);
 //API Endpoints
-app.get("/",(req,res)=> res.status(200).send("Hello there"));
+
+
+app.get("/", (req, res) => {
+    res.json({
+        massege: "Hello there"
+    });
+});
+
+
+
+
+// Routes
+app.use("/api/users",users);
 
 //Listener
-app.listen(port, () => console.log("server started on port "+port));
+app.listen(port, () => console.log(`server started on port ${port}`));
