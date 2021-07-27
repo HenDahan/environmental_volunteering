121d3f28cd50765cc0c4d622b9f81270ce93be22
TzurielYeffet <70668027+TzurielYeffet@users.noreply.github.com>
2021-07-27 19:40:56 +0300

    Add files via upload

--- app-backend/server.js
+++ app-backend/server.js
@@ -32,9 +32,16 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(cors(corsOptions));
+app.use(cors());
+
+app.use("/",require("./routes/api/users"))
+
+
 
 // DB config
+
+
+
 
 const User = require('./db/users.schema');
 
